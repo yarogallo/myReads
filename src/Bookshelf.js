58863119f13625d3e70c93363e40e830c1c2fc2a
yanisleidi <yaroga88@yahoo.com>
2018-03-02 01:13:27 -0800

    Create Bookshelf component

---
+++ src/Bookshelf.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Book from './Book';
+
+class Bookshelf extends Component{
+	render(){
+		const booksInShelf = this.props.books;
+		const bookshelfHeader = this.props.shelfHeader;
+		return(
+			<div className="bookshelf">
+				<header>
+					<h2 className="bookshelf-title">{bookshelfHeader}</h2>
+				</header>
+				<div className="bookshelf-books">
+					<ol className="books-grid">
+						{ booksInShelf.map( book => (<li key={book.title}>
+							<Book
+								title={book.title}
+								author={book.author}
+								shelf={book.shelf}
+								imgUrl={book.imgUrl}
+							/>
+						</li>)) }
+					</ol>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Bookshelf;
