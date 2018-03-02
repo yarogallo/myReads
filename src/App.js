96b2992ee40b851686b60a6501638ddd273c1aaf
yanisleidi <yaroga88@yahoo.com>
2018-03-01 17:39:55 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return ( <
+            div >
+            <header className = "App-header" >
+            <h1 > My Reads </h1> </header> </div>
+        );
+    }
+}
+
+export default App;
