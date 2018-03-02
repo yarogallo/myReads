cc1aac0d933db18eb0d96196cc015e571a6d76e5
yanisleidi <yaroga88@yahoo.com>
2018-03-02 15:32:22 -0800

    Include ListBooks and SearchBooks in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import ListBooks from './ListBooks';
+import SearchBooks from './SearchBooks';
 
 class App extends Component {
+	shelfList = [{
+		header: 'Currently Reading',
+		books: [{title: 'one', author: 'two', shelf:'reads', imgUrl:'icons/yani.jpg'},
+		{title: 'lolo', author: 'two', shelf:'reads', imgUrl:'icons/yani.jpg'},
+		{title: 'sera', author: 'two', shelf:'reads', imgUrl:'icons/yani.jpg'}],
+	},{
+		header: 'Want to Read',
+		books: [{title: 'one', author: 'two', shelf:'reads', imgUrl:'icons/yani.jpg'}]
+	},{
+		header: 'Read',
+		books: [{title: 'one', author: 'two', shelf:'reads', imgUrl:'icons/yani.jpg'},{title: 'lolo', author: 'two', shelf:'reads', imgUrl:'icons/yani.jpg'}, {title: 'one', author: 'two', shelf:'reads', imgUrl:'icons/yani.jpg'},{title: 'lolo', author: 'two', shelf:'reads', imgUrl:'icons/yani.jpg'}]
+	}]
+	
     render() {
         return ( 
 		<div className="app">
-			<div className="list-books">
-            	<header className = "list-books-title" >
-            		<h1>MyReads</h1> 	
-				</header>
-			</div>
+			<ListBooks shelfList={this.shelfList}/>
+			<SearchBooks />
 		</div>
         );
     }
