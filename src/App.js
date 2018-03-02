1f56c954e59b96a34e512bb3a0b07625ef03c31d
yanisleidi <yaroga88@yahoo.com>
2018-03-01 22:37:10 -0800

    Add css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 
 class App extends Component {
     render() {
-        return ( <
-            div >
+        return ( <div>
             <header className = "App-header" >
             <h1 > My Reads </h1> </header> </div>
         );
