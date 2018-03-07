b050190a4e28cbf72b75c2413656b15303a48081
yanisleidi <yaroga88@yahoo.com>
2018-03-07 15:00:30 -0800

    Make Book a stateless functional component

--- src/Book.js
+++ src/Book.js
@@ -1,29 +1,33 @@
-import React, {Component} from 'react';
-import Selector from './Selector'
+import React from 'react';
+import Selector from './Selector';
+import propTypes from 'prop-types'
 
-class Book extends Component {
-	handleSelectShelf(evt, book) {
-		this.props.onSelectShelf(evt.target.value, book);
-		evt.preventDefault();
-	}
+const Book = function({book, onSelectShelf}){
+	const title = book.title;
+	const authors = book.authors || ['No authors to show'];
+	const imageLink = book.imageLinks && book.imageLinks.smallThumbnail;
+	const shelf = book.shelf;
 	
-	render(){
-		const {book} = this.props;
-		const title = book.title;
-		const authors = book.authors || ['No authors to show'];
-		const imageLink = book.imageLinks? book.imageLinks.smallThumbnail: './icons/image.svg';
-		const shelf = book.shelf;
-		return(
-			<div className='book' onChange={(evt) => this.handleSelectShelf(evt, book)}>
-				<div className='book-top'>
+	const handleSelectShelf = function(evt, book) {
+				onSelectShelf(evt.target.value, book);
+		 		evt.preventDefault();
+			}
+	
+	return (
+		<div className='book' onChange={(evt) => handleSelectShelf(evt, book)}>
+ 				<div className='book-top'>
 					<div className='book-cover' style={{backgroundImage : `url("${imageLink}")`}}></div>
-					<Selector shelf={shelf}/>		
-				</div>
-				<div className="book-title">{title}</div>
-				{ authors.map( author => ( <div className='book-authors' key={author}>{author}</div>)) }
-			</div>
-		);
-	}
-}
+ 					<Selector shelf={shelf}/>		
+ 				</div>
+ 				<div className="book-title">{title}</div>
+ 				{ authors.map( author => ( <div className='book-authors' key={author}>{author}</div>)) }
+ 			</div>
+	);
+};
+
+Book.propTypes = {
+	book: propTypes.object.isRequired,
+	onSelectShelf: propTypes.func.isRequired
+};
 
 export default Book;
