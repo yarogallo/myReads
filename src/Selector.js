ea8e78691d17987e1df1f4ec30f5cc348705fd1a
yanisleidi <yaroga88@yahoo.com>
2018-03-07 13:40:16 -0800

    Make Selector a stateless functional componenet

--- src/Selector.js
+++ src/Selector.js
@@ -1,19 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import propTypes from 'prop-types'
 
-class Selector extends Component {
-	render(){
-		return (
-			<div className='book-shelf-changer'>
-				<select defaultValue={this.props.shelf || 'noneShelf'}>
-					<option value='none' disabled={true}>Move to ...</option>
-					<option value='currentlyReading'>Currently Reading</option>
-					<option value='wantToRead'>Want to Read</option>
-					<option value='read'>Read</option>
-					<option value='noneShelf'>None</option>
-				</select>
-			</div>
-		);
-	}
-}
+const Selector = function({shelf='noneShelf'}){
+	return (
+		<div className='book-shelf-changer'>
+			<select defaultValue={shelf}>
+				<option value='none' disabled={true}>Move to ...</option>
+				<option value='currentlyReading'>Currently Reading</option>
+				<option value='wantToRead'>Want to Read</option>
+				<option value='read'>Read</option>
+				<option value='noneShelf'>None</option>
+			</select>
+		</div>
+	);
+};
+
+Selector.propTypes = {
+	shelf: propTypes.string	
+};
 
 export default Selector;
