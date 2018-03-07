7318b300fbf16876398d3e37e40b17bb3da341c8
yanisleidi <yaroga88@yahoo.com>
2018-03-07 15:09:36 -0800

    Make Booksehelf a stateless functional component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,28 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Book from './Book';
+import propTypes from 'prop-types'
 
-class Bookshelf extends Component{
-	
-	render(){
-		const {header, books, onSelectShelf}= this.props;
-		return(
-			<div className="bookshelf">
-				<header>
-					<h2 className="bookshelf-title">{header}</h2>
-				</header>
-				<div className="bookshelf-books">
-					<ol className="books-grid">
-						{ books.map( book => (
-							<li key={book.id} >
-								<Book book={book}
-									  onSelectShelf={onSelectShelf}
-								/>
-							</li>)) }
-					</ol>
-				</div>
+const Bookshelf = function({header, books, onSelectShelf}){
+	return(
+		<div className="bookshelf">
+			<header>
+				<h2 className="bookshelf-title">{header}</h2>
+			</header>
+			<div className="bookshelf-books">
+				<ol className="books-grid">
+					{ books.map( book => (
+						<li key={book.id} >
+							<Book book={book} onSelectShelf={onSelectShelf} />
+						</li>
+					)) }
+				</ol>
 			</div>
-		);
-	}
+		</div>
+	);
+}
+
+Bookshelf.propTypes = {
+	header: propTypes.string.isRequired,
+	books: propTypes.array.isRequired,
+	onSelectShelf: propTypes.func.isRequired
 }
 
 export default Bookshelf;
