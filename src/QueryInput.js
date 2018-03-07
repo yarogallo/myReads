96cddc6363120b4cbccdeb5bd0774a2cbb883aa9
yanisleidi <yaroga88@yahoo.com>
2018-03-07 15:34:28 -0800

    Make QueryInput stateless functional component

--- src/QueryInput.js
+++ src/QueryInput.js
@@ -1,22 +1,26 @@
-import React, {Component} from 'react';
+import React  from 'react';
 import {Link} from 'react-router-dom';
+import propTypes from 'prop-types';
 
-class QueryInput extends Component {
-	render(){
-		const {query, onChangeQuery} = this.props;
-		return(
-			<div className="search-books-bar">
-				<Link to='/' className="close-search">Close</Link>
-				<div className="search-books-input-wrapper">
-					<input type="text" placeholder="Search by title or author" 
-						value={query}
-						onChange={(evt)=>{
-							onChangeQuery(evt.target.value);
-						}}/>
-              	</div>
-			</div>
-		);
+const QueryInput = function({query, onChangeQuery}){
+	const onChangeQueryHandler = (newQuery)=>{
+		onChangeQuery(newQuery)
 	}
+	return(
+		<div className="search-books-bar">
+			<Link to='/' className="close-search">Close</Link>
+			<div className="search-books-input-wrapper">
+				<input type="text" placeholder="Search by title or author" 
+					value={query} onChange={ evt=> onChangeQueryHandler(evt.target.value) }
+				/>
+            </div>
+		</div>
+	);
+}
+
+QueryInput.propTypes = {
+	query: propTypes.string.isRequired,
+	onChangeQuery: propTypes.func.isRequired
 }
 
 export default QueryInput;
